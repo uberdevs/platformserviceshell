72c1d04dad74555997fc6edb8edadbfb09efb442
quantumdev2380 <manishnarayan2380@gmail.com>
2017-10-04 11:01:38 -0700

    Add files via upload

---
+++ DotNet/Plugin.Shell.Contracts/IDataAccessPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Shell.Contracts
+{
+    public interface IDataAccessPlugin
+    {
+        string Name { get; }
+        void ConnectToDatabase();
+    }
+}
---
+++ DotNet/Plugin.Shell.UI/DataAccessPluginLoader.cs
@@ -0,0 +1,69 @@
+﻿using Plugin;
+using Plugin.Shell.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Shell.UI
+{
+    public static class DataAccessPluginLoader
+    {
+        public static ICollection<IDataAccessPlugin> LoadPlugins(string path)
+        {
+            string[] dllFileNames = null;
+
+            if (Directory.Exists(path))
+            {
+                dllFileNames = Directory.GetFiles(path, "*.dll");
+
+                ICollection<Assembly> assemblies = new List<Assembly>(dllFileNames.Length);
+                foreach (string dllFile in dllFileNames)
+                {
+                    AssemblyName an = AssemblyName.GetAssemblyName(dllFile);
+                    Assembly assembly = Assembly.Load(an);
+                    assemblies.Add(assembly);
+                }
+
+                Type pluginType = typeof(IDataAccessPlugin);
+                ICollection<Type> pluginTypes = new List<Type>();
+                foreach (Assembly assembly in assemblies)
+                {
+                    if (assembly != null)
+                    {
+                        Type[] types = assembly.GetTypes();
+
+                        foreach (Type type in types)
+                        {
+                            if (type.IsInterface || type.IsAbstract)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                if (type.GetInterface(pluginType.FullName) != null)
+                                {
+                                    pluginTypes.Add(type);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                ICollection<IDataAccessPlugin> plugins = new List<IDataAccessPlugin>(pluginTypes.Count);
+                foreach (Type type in pluginTypes)
+                {
+                    IDataAccessPlugin plugin = (IDataAccessPlugin)Activator.CreateInstance(type);
+                    plugins.Add(plugin);
+                }
+
+                return plugins;
+            }
+
+            return null;
+        }
+    }
+}
