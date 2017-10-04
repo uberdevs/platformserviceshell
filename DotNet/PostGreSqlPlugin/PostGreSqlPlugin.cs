f012146e6daa083b2926ba188200cc411dba9e44
quantumdev2380 <manishnarayan2380@gmail.com>
2017-10-04 11:05:32 -0700

    Add files via upload

---
+++ DotNet/PostGreSqlPlugin/PostGreSqlPlugin.cs
@@ -0,0 +1,51 @@
+﻿using Plugin;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Shell.Contracts;
+
+namespace PostGreSqlPlugin
+{
+    public class PostGreSqlPlugin : IDataAccessPlugin
+    {
+        #region IPlugin Members
+
+        public string Name
+        {
+            get
+            {
+                return "PostGreSql Plugin"; //
+            }
+        }
+
+        public void ConnectToDatabase()
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection("Host=;Username=;Password=;Database="))
+                {
+                    connection.Open();
+                    using (var command = new NpgsqlCommand("", connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+
+}
---
+++ DotNet/SOAPPlugin/SOAPPlugin.cs
@@ -0,0 +1,62 @@
+﻿using Plugin.Shell.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SOAPPlugin
+{
+    public class SOAPPlugin : IWebServicePlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "SOAP Plugin";
+            }
+        }
+
+        public void InvokeWebService()
+        {
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(@"http://localhost:56405/WebService1.asmx?op=HelloWorld");
+                webRequest.Headers.Add(@"SOAP:Action");
+                webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+                webRequest.Accept = "text/xml";
+                webRequest.Method = "POST";
+
+                HttpWebRequest request = webRequest;
+                XmlDocument soapEnvelopeXml = new XmlDocument();
+                soapEnvelopeXml.LoadXml(@"<?xml version=""1.0"" encoding=""utf-8""?>
+                <soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
+                  <soap:Body>
+                    <HelloWorld xmlns=""http://tempuri.org/"" />
+                  </soap:Body>
+                </soap:Envelope>");
+
+                using (Stream stream = request.GetRequestStream())
+                {
+                    soapEnvelopeXml.Save(stream);
+                }
+
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (StreamReader rd = new StreamReader(response.GetResponseStream()))
+                    {
+                        string soapResult = rd.ReadToEnd();
+                        Console.WriteLine(soapResult);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
